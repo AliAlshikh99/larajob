5e9dfde7cd85d21bb414bbfbb057bf5f06cc37f2
AliAlshikh99 <Ali.Alshikh99@outlook.com>
2023-06-15 12:37:16 +0200

    edit in blade file

--- resources/views/Jobs.blade.php
+++ resources/views/Jobs.blade.php
@@ -4,7 +4,7 @@
 
     <div class="lg:grid lg:grid-cols-2  gap-4 space-y-4 md:space-y-0 mx-4">
         @if (count($jobs) == 0)
-            <p class="text-5xl m-5 text-center text-neutral-950">Not Jobs Found....</p>
+            <p class="text-5xl m-5 text-center text-neutral-950">Jobs Not Found....</p>
             <a href="/" class="flex justify-end items-end">
                
         @else
